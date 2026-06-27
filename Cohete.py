4eb012f33cbaa61dc5c45b7bf41caf63b0f02cf1
SrMatuche <paddingtonsuarez@gmail.com>
2026-06-27 10:54:35 -0500

    Create Cohete.py

---
+++ Cohete.py
@@ -0,0 +1,17 @@
+# projecto del cohete
+import time
+
+print("Este es mi projecto del cohete que lo estoy avanzando desde hoy 6-02-2026")
+
+altura=0
+cuenta = 5
+
+print("iniciando lanzamiento")
+
+while cuenta >= 0:
+   print(cuenta)
+   cuenta=cuenta -1
+   time.sleep(1)
+   
+
+print("despeguando")   
